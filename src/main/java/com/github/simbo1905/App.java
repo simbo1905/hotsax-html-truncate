bbe7208a3a851e1f25fd1078fdc52f1e7cd14bb2
simbo1905 <simbo1905@60hertz.com>
2015-02-19 21:56:08 +0000

    tweaks

--- src/main/java/com/github/simbo1905/App.java
+++ src/main/java/com/github/simbo1905/App.java
@@ -9,53 +9,64 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-public class App {
-	public static void main(String[] args) throws Exception {
-		XMLReader parser = XMLReaderFactory
-				.createXMLReader("hotsax.html.sax.SaxParser");
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	XMLReader parser = XMLReaderFactory.createXMLReader("hotsax.html.sax.SaxParser");
+    	
+    	final StringBuilder builder = new StringBuilder();
+    	
+    	ContentHandler handler = new DoNothingContentHandler(){
 
-		final StringBuilder builder = new StringBuilder();
-
-		ContentHandler handler = new DoNothingContentHandler() {
-
-			StringBuilder wholeTag = new StringBuilder();
-
+    		StringBuilder wholeTag = new StringBuilder();
+    		boolean hasText = false;
+    		boolean hasElements = false;
+    		String lastStart = "";
+    		
 			@Override
 			public void characters(char[] ch, int start, int length)
 					throws SAXException {
-				wholeTag.append(new String(ch, start, length));
+				String text = (new String(ch, start, length)).trim();
+				wholeTag.append(text);
+				hasText = true;
 			}
 
 			@Override
 			public void endElement(String namespaceURI, String localName,
 					String qName) throws SAXException {
-				wholeTag.append("</" + localName + ">");
+				if( !hasText && !hasElements && lastStart.equals(localName)) {
+					builder.append("<"+localName+"/>");
+				} else {
+					wholeTag.append("</"+ localName +">");
+					builder.append(wholeTag.toString());
+				}
+				
+				wholeTag = new StringBuilder();
+				hasText = false;
+				hasElements = false;
 			}
 
 			@Override
 			public void startElement(String namespaceURI, String localName,
 					String qName, Attributes atts) throws SAXException {
-				wholeTag.append("<" + localName);
-				for (int i = 0; i < atts.getLength(); i++) {
-					wholeTag.append(" " + atts.getLocalName(i) + "='"
-							+ atts.getValue(i) + "'");
+				wholeTag.append("<"+ localName);
+				for( int i = 0; i < atts.getLength(); i++) {
+					wholeTag.append(" "+atts.getQName(i)+"='"+atts.getValue(i)+"'");
+					hasElements = true;
 				}
 				wholeTag.append(">");
-				builder.append(wholeTag.toString());
-				wholeTag = new StringBuilder();
+				lastStart = localName;
+				hasText = false;
 			}
-
-		};
+    		
+    	};
 		parser.setContentHandler(handler);
+		
+		//parser.parse(new InputSource( new StringReader( "<div>this is the <em>end</em> my <br> friend <a href=\"whatever\">some link</a>" ) ));
+		parser.parse(new InputSource( new StringReader( "<img src=\"http://d2qxdzx5iw7vis.cloudfront.net/34775606.jpg\" />\n<br/><a href=\"htt" ) ));
+		
+        System.out.println( builder.toString() );
 
-		// parser.parse(new InputSource( new StringReader(
-		// "<div>this is the <em>end</em> my <br> friend <a href=\"whatever\">some link</a>"
-		// ) ));
-		parser.parse(new InputSource(
-				new StringReader(
-						"<img src=\"http://d2qxdzx5iw7vis.cloudfront.net/34775606.jpg\" />\n<br/><a href=\"htt")));
-
-		System.out.println(builder.toString());
-
-	}
+    }
 }
